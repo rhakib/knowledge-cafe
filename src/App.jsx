e413c14ed02bd80d42f92c14cd57301e0b2e78c2
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2023-09-14 21:30:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1 className='text-4xl bg-red-400'>Knowledge Cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
